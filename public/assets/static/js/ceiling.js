8d425860e48066475533b1c12f8bfe8da06ccddf
Anis Moubarik <anis.moubarik@helsinki.fi>
2015-10-26 12:39:04 +0200

    Re-organized d3 code and added some initial transitions  Changes to be committed: 	modified:   public/assets/static/css/ceiling.css 	modified:   public/assets/static/js/ceiling.js

--- public/assets/static/js/ceiling.js
+++ public/assets/static/js/ceiling.js
@@ -8,71 +8,99 @@ var interval = setInterval(function(){
   console.log("Checking for .chart");
   if(chart[0] != null){
     clearInterval(interval);
-    console.log(chart);
     drawBarChart(chart);
   }
 }, 100);
 
-
+var svg, xAxis, yAxis, x, y, width, height;
 function drawBarChart(c){
 
-  var margin = {top: 20, right: 30, bottom: 30, left: 40},
-    width = 500 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+  var margin = {top: 20, right: 30, bottom: 30, left: 40};
+  width = 500 - margin.left - margin.right;
+  height = 400 - margin.top - margin.bottom;
 
-  var x = d3.scale.ordinal().rangeRoundBands([0, width],.1);
-  var y = d3.scale.linear().range([height, 0]);
+  x = d3.scale.ordinal().rangeRoundBands([0, width],.1);
+  y = d3.scale.linear().range([height, 0]);
 
 
 
-  var svg = c.append("svg")
+  svg = c.append("svg")
     .attr("width", width+margin.left+margin.right)
     .attr("height", height+margin.top+margin.bottom)
     .append("g")
     .attr("transform", "translate("+margin.left + ","+ margin.top +")");
 
-  var xAxis = d3.svg.axis()
+  xAxis = d3.svg.axis()
     .scale(x)
     .orient("bottom");
-  var yAxis = d3.svg.axis()
+  yAxis = d3.svg.axis()
     .scale(y)
     .orient("left")
     .ticks(5, "");
+
+  svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0,"+ height + ")");
+
+  svg.append("g")
+    .attr("class", "y axis")
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .attr("text-anchor", "end")
+    .text("Mood")
 
   d3.json("http://localhost:3000/dev/assets/static/js/testdata.json", function(error, data){
     x.domain(data.map(function(d){ return d.mood; }));
     y.domain([0, d3.max(data, function(d){ return d.value; })]);
 
-    svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0,"+ height + ")")
-      .call(xAxis);
-
-    svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .attr("text-anchor", "end")
-      .text("Mood")
-
-    svg.selectAll("bar")
-      .data(data)
-      .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) {
-        return x(d.mood);
-      })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d){
-        return y(d.value);
-      })
-      .attr("height", function(d){
-        return height - y(d.value);
-      });
+    setTimeout(function(){
+      replay(data);
+    }, 1000)
   });
+}
+
+function replay(data){
+  var count = 5;
+  var slices = [];
+  for (var i = 0; i <= data.length; i++){
+    slices.push(data.slice(0, i));
+  }
+  slices.forEach(function(slice, index){
+    setTimeout(function(){
+      draw(slice);
+    }, index * 300);
+  });
+}
+
+function draw(data){
+  x.domain(data.map(function(d){ return d.mood; }));
+  y.domain([0, d3.max(data, function(d){ return d.value; })]);
+
+  svg.select('.x.axis').transition().duration(300).call(xAxis);
+  svg.select('.y.axis').transition().duration(300).call(yAxis);
+
+  var bars = svg.selectAll('.bar').data(data, function(d){ return d.mood; });
+
+  bars.exit()
+    .transition()
+    .duration(300)
+    .attr("y", y(0))
+    .attr("height", height - y(0))
+    .style("fill-opacity", 1e-6)
+    .remove();
+
+  bars.enter().append("rect")
+    .attr("class", "bar")
+    .attr("y", y(0))
+    .attr("height", height - y(0));
+
+  bars.transition().duration(300).attr("x", function(d){ return x(d.mood); })
+    .attr("width", x.rangeBand())
+    .attr("y", function(d){ return y(d.value); })
+    .attr("height", function(d){ return height - y(d.value); });
+
 
 }
 
